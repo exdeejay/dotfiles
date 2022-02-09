451dadf8561f1f71b2a80d15a5933342266d16ca
exdeejay <13634296+exdeejay@users.noreply.github.com>
2022-02-09 18:23:25 -0500

    install changes, fzf, vim background

--- install.sh
+++ install.sh
@@ -3,47 +3,69 @@
 readYN() {
 	selection="notset"
 	while [ "${selection,,}" != "y" -a "${selection,,}" != "n" -a "$selection" != "" ]; do
-		printf "$1: " >&2
+		printf "$1" >&2
+		[ "${2,,}" == "y" ] && printf "[Yn]" >&2 || printf "[yN]" >&2
+		printf ": " >&2
 		read -e selection
 	done
-	case "${selection,,}" in
-		y) echo 1;;
-		n) echo 0;;
-		*) echo 2;;
-	esac
+	[ "$selection" != "" ] && echo "${selection,,}" || echo "${2,,}"
 }
 
-cd "$(dirname $0)"
-
-DOTFILES_DIR="$PWD"
-
-echo "Backing up existing dotfiles to $(basename $PWD)/backup"
-echo "Installing dotfiles..."
-if [ ! -d "backup" ]; then
-	mkdir backup
-fi
-for dotfile in .*; do
-	case $dotfile in
-		.|..|.git|.gitignore|.gitmodules) continue;;
-		*)
-			if [ -a "$HOME/$dotfile" ]; then
-				if [[ "$(dirname "$(readlink "$HOME/$dotfile")")" == "$PWD" ]]; then
-					break
-				fi
-				rm -rf "backup/$dotfile.bak"
-				mv -v "$HOME/$dotfile" "backup/$dotfile.bak"
-			fi
-			if [ "$OS_TYPE" == "msys" ]; then
-				cp "$DOTFILES_DIR/$dotfile" "$HOME/$dotfile"
-			else
-				ln -sv "$DOTFILES_DIR/$dotfile" "$HOME/$dotfile"
-			fi
-			;;
-	esac
+SOURCE=${BASH_SOURCE[0]}
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+	SOURCE=$(readlink "$SOURCE")
+	[[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
+DOTFILES_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+cd "$DOTFILES_DIR"
+
+if [ "$OS_TYPE" = "msys" ]; then
+	echo "!!WARNING!! MSYS is not fully supported, use at your own risk!"
+	[ $(readYN "Continue?" 'n') == 'n' ] && exit
+fi
+
+echo "Backing up existing dotfiles to $(basename $DOTFILES_DIR)/backup"
+echo "Installing dotfiles..."
+
+[ ! -d "backup" ] && mkdir backup
+[ ! -d "$HOME/.local/bin" ] && mkdir -p "$HOME/.local/bin"
+[ ! -d "backup/bin" ] && mkdir backup/bin
+
+copyWithBackup() {
+
+	src_dir=$1
+	target_dir=$2
+	ignored=$3
+
+	cd -P "$src_dir"
+
+	for file in $(ls -A); do
+		[[ "$ignored" =~ "$file" ]] && continue
+		# Check if file already exists
+		if [ -a "$target_dir/$file" ]; then
+			# Don't back up file if it's already symlinked here
+			[ "$(dirname "$(readlink "$target_dir/$file")")" == "$PWD" ] && continue
+			relative_path=$(realpath --relative-to="$DOTFILES_DIR" "$src_dir")
+			# Copy file and overwrite old backup
+			rm -rf "$DOTFILES_DIR/backup/$relative_path/$file.bak"
+			mv -v "$target_dir/$file" "$DOTFILES_DIR/backup/$relative_path/$file.bak"
+		fi
+		# Copy file if this is msys (no symlinks)
+		if [ "$OS_TYPE" == "msys" ]; then
+			cp "$file" "$target_dir/$file"
+		else
+			ln -sv "$DOTFILES_DIR/$relative_path/$file" "$target_dir/$file"
+		fi
+	done
+}
+
+copyWithBackup "." "$HOME" ".git .gitignore .gitmodules backup TODO.md install.sh uninstall.sh"
+copyWithBackup "bin" "$HOME/.local/bin"
 
 
-if [[ $(readYN "set git username and email? [yN]") == 1 ]]; then
+if [ $(readYN "set git username and email?" 'n') == 'y' ]; then
 	printf "Username: "
 	read -e git_username
 	printf "Email: "
@@ -58,7 +80,6 @@ fi
 if [ ! -d "$HOME/.bash_include" ]; then
 	mkdir "$HOME/.bash_include"
 fi
-touch "$HOME/.tmux.conf.local"
 
 echo "done"
 
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
-cd "$(dirname $0)"
-DOTFILES_DIR="$(pwd)"
+readYN() {
+	selection="notset"
+	while [ "${selection,,}" != "y" -a "${selection,,}" != "n" -a "$selection" != "" ]; do
+		printf "$1" >&2
+		[ "${2,,}" == "y" ] && printf "[Yn]" >&2 || printf "[yN]" >&2
+		printf ": " >&2
+		read -e selection
+	done
+	[ "$selection" != "" ] && echo "${selection,,}" || echo "${2,,}"
+}
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+	SOURCE=$(readlink "$SOURCE")
+	[[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DOTFILES_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+cd "$DOTFILES_DIR"
 
 if [ "$OSTYPE" == "msys" ]; then
 	echo "!!! VERY DANGER !!!"
@@ -13,15 +31,19 @@ fi
 
 echo "Removing dotfiles..."
 
-for link in $(find "$HOME" -maxdepth 1 -type l); do
-	if [ "$(dirname $(realpath "$link"))" == "$DOTFILES_DIR" ]; then
-		rm -rf "$link"
-		echo "Removed $(basename "$link")"
-	fi
-done
+removeLinks() {
+	target_dir=$1
 
-unset DOTFILES_DIR
-unset DOTFILES
+	for link in $(find "$target_dir" -maxdepth 1 -type l); do
+		if [ "$(dirname $(realpath "$link"))" == "$DOTFILES_DIR" ]; then
+			rm -rf "$link"
+			echo "Removed $(basename "$link")"
+		fi
+	done
+}
+
+removeLinks "$HOME"
+removeLinks "$HOME/.local/bin"
 
 echo "done"
 
