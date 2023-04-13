3b6180dd3f2239394a3571e818600febea85c306
exdeejay <13634296+exdeejay@users.noreply.github.com>
2023-04-12 20:34:25 -0400

    fix nvm install, path

--- bash_include/path.sh
+++ bash_include/path.sh
@@ -4,7 +4,12 @@
 [[ -d "$HOME/.local/bin" ]] && export PATH="$HOME/.local/bin:$PATH"
 
 # only enable `bin` if we can actually run them
-[[ "$(uname -p)" == "x86_64" ]] && export PATH="$ROOT_DIR/bin:$PATH"
+[[ "$(uname -m)" == "x86_64" ]] && export PATH="$ROOT_DIR/bin:$PATH"
 
 export PATH="$ROOT_DIR/scripts:$PATH"
 
+if [[ -d "$HOME/.nvm" ]]; then
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
--- modules/nvm/install.sh
+++ modules/nvm/install.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 
 echo "v0.39.2"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash &&
+	sed -i "$(($(wc -l < "$HOME/.bashrc") - 3)),\$d" "$HOME/.bashrc"
