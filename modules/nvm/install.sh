c9db49f9a327be921ab081268ac05b9f75f23682
exdeejay <13634296+exdeejay@users.noreply.github.com>
2023-08-10 00:24:10 -0400

    add node install script, vimplug convenience installer

--- modules/nvm/install.sh
+++ modules/nvm/install.sh
@@ -1,7 +1,12 @@
 #/bin/bash
 
-git clone https://github.com/nvm-sh/nvm.git ~/.nvm
-pushd ~/.nvm 2>/dev/null
-git checkout v0.39.4
-popd 2>/dev/null
+git clone https://github.com/nvm-sh/nvm.git $HOME/.nvm
+pushd $HOME/.nvm &>/dev/null
+git checkout v0.39.4 &>/dev/null
+popd &>/dev/null
+
+if [[ $(readYN "[+] Install latest version of node?" 'y') == 'y' ]]; then
+	source $HOME/.nvm/nvm.sh
+	nvm install node
+fi
 
